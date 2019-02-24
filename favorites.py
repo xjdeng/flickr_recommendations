1ff06168cc8bfd219b5023b5d701a0e2b90a3c37
xjdeng <xjd001@gmail.com>
2019-02-24 17:55:52 -0600

    Get myself and list of favorites

--- favorites.py
+++ favorites.py
@@ -1,4 +1,5 @@
 import flickr_api
+from flickr_api.api import flickr
 
 def _set_keys(keyfile = "default.key"):
     f = open(keyfile, 'r')
@@ -10,8 +11,23 @@ def _set_keys(keyfile = "default.key"):
 def _set_auth(authfile = "default.auth"):
     flickr_api.set_auth_handler(authfile)
     return flickr_api
+
+def myself():
+    return flickr_api.test.login()
     
 def start(keyfile = "default.key", authfile = "default.auth"):
     _set_keys(keyfile)
     _set_auth(authfile)
-    return flickr_api
+    flickr_api.enable_cache()
+    return flickr_api,flickr
+
+def favorites():
+    me = myself()
+    favs = me.getFavorites(per_page = 500)
+    newfavs = favs
+    i = 1
+    while len(newfavs) != 0:
+        i += 1
+        newfavs = me.getFavorites(per_page = 500, page = i)
+        favs += newfavs
+    return favs
