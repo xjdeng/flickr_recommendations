e4767c6c2d3f00fc76f3a832f7198227d0d64bcb
xjdeng <xjd001@gmail.com>
2019-02-24 18:16:26 -0600

    Use an _exhaustive function to get user favorites

--- favorites.py
+++ favorites.py
@@ -12,6 +12,21 @@ def _set_auth(authfile = "default.auth"):
     flickr_api.set_auth_handler(authfile)
     return flickr_api
 
+def _exhaustive(myfunction):
+    results = myfunction(per_page = 500)
+    new = results
+    i = 1
+    while len(new) != 0:
+        i += 1
+        new = myfunction(per_page = 500, page = i)
+        results += new
+    return results
+
+def favorites(user = None):
+    if user is None:
+        user = myself()
+    return _exhaustive(user.getFavorites)
+
 def myself():
     return flickr_api.test.login()
     
@@ -20,14 +35,3 @@ def start(keyfile = "default.key", authfile = "default.auth"):
     _set_auth(authfile)
     flickr_api.enable_cache()
     return flickr_api,flickr
-
-def favorites():
-    me = myself()
-    favs = me.getFavorites(per_page = 500)
-    newfavs = favs
-    i = 1
-    while len(newfavs) != 0:
-        i += 1
-        newfavs = me.getFavorites(per_page = 500, page = i)
-        favs += newfavs
-    return favs
