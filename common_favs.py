7873fcb6aa3f33f47d02e456e4ddf0744e604897
xjdeng <xjd001@gmail.com>
2019-03-01 22:23:14 -0600

    Add method to convert dict to Pandas

--- common_favs.py
+++ common_favs.py
@@ -4,13 +4,22 @@ except ImportError:
     import favorites
 
 from collections import defaultdict
+import pandas as pd
 
-def run(favs = None):
+def run(favs = None, verbose = False):
     if favs is None:
         favs = favorites.favorites()
     users = defaultdict(lambda:0)
-    for f in favs:
+    for i,f in enumerate(favs):
         fans = favorites._exhaustive(f.getFavorites)
+        n = len(favs)
+        if verbose == True:
+            print("Trying {} of {}".format(i, n))
         for fan in fans:
-            users[fan] += 1
+            users[fan.username] += 1
     return users
+
+def to_pd(in_dict):
+    index = list(in_dict.keys())
+    values = [in_dict[i] for i in index]
+    return pd.Series(values, index = index)
